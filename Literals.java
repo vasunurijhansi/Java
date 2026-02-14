4f29426925895bbe18bbf9e28629db9e08d31b1e
vasunurijhansi <vasunurijhansi@gmail.com>
2026-02-14 13:09:48 +0530

    Add files via upload

---
+++ Literals.java
@@ -0,0 +1,23 @@
+public class Literals
+{
+    public static void main(String args[])
+    {
+        int a=40;
+        int b=054;
+        int c=0x3a;
+        int e=0b1;
+        float f=12.897f;
+        double d=10.43567d;
+        char ch='_';
+        boolean value=true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(ch);
+        System.out.println(value);
+
+    }
+}
