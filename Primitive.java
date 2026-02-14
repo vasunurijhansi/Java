f8719cd2ba5a2c6d7171de042c7ec863dfa9fb8d
vasunurijhansi <vasunurijhansi@gmail.com>
2026-02-14 13:10:25 +0530

    Add files via upload

---
+++ Primitive.java
@@ -0,0 +1,24 @@
+public class Primitive
+{
+    public static void main(String args[])
+    {
+        byte b=125;
+        short s=178;
+        int c=1345;
+        long l=12345;
+        float f=19.654f;
+        double d=12.76453;
+        char ch='s';
+        char cha='*';
+        boolean value=true;
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(c);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(ch);
+        System.out.println(cha);
+        System.out.println(value);
+    }
+}
