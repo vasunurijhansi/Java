51b1bf21c2f2e4ba0845b9bbd638b064fc87845e
vasunurijhansi <vasunurijhansi@gmail.com>
2026-02-14 13:11:08 +0530

    Add files via upload

---
+++ Swaping_methods.java
@@ -0,0 +1,15 @@
+public class Swaping_methods
+{
+    public static void main(String args[])
+    {
+        int a=4;
+        int b=7;
+        int temp;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+        
+    }
+}
