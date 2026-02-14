e9652f1d4ee2e1fd498cc45ab40472281355e69b
vasunurijhansi <vasunurijhansi@gmail.com>
2026-02-14 13:09:27 +0530

    Add files via upload

---
+++ Arthemtic_swaping.java
@@ -0,0 +1,12 @@
+public class Arthemtic_swaping
+{
+    public static void main(String args[])
+    {
+        int a=2;
+        int b=3;
+        a=a+b
+        System.out.println("a=a+b");
+        System.out.println("b=a-b");
+        System.out.println("a=a-b");
+    }
+}
