c600f3d3e97936c72a2ebd2114d5e5e36f68004c
vasunurijhansi <vasunurijhansi@gmail.com>
2026-02-14 13:10:42 +0530

    Add files via upload

---
+++ Protein.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		int Y=sc.nextInt();
+		if(X>=Y)
+		{
+		    System.out.println("yes");
+		}
+        else{
+            System.out.println("no");
+        }
+	}
+}
