ae1e38c10e562f317a5c0350fce0e10543510c32
vasunurijhansi <vasunurijhansi@gmail.com>
2026-02-14 13:06:30 +0530

    Add files via upload

---
+++ swaping.java
@@ -0,0 +1,14 @@
+public class swaping {
+    public static void main (String args[])
+    {
+        int a=6;
+        int b=3;
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.pritnln("a="+a);
+        System.out.println("b="+b);
+
+    }
+
+    }
