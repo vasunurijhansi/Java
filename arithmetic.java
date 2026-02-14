a0aaaab9e19270962c528e03ac6f1459116dc3f3
vasunurijhansi <vasunurijhansi@gmail.com>
2026-02-14 13:05:26 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,14 @@
+public  class arithmetic {
+    public static void main(String arg[])
+    {
+        int a=23;
+        int b=12;
+        System.out.println(a+b);
+        System.out.println(a==b);
+        System.out.println(+a);
+        System.out.println(-b);
+        System.out.println(a=23);
+
+    }
+
+  }
