b05b437d39ee6b3be252340cd5bbb4be33805f44
vasunurijhansi <vasunurijhansi@gmail.com>
2026-02-28 12:30:29 +0530

    Add files via upload

---
+++ Squarepattern.java
@@ -0,0 +1,22 @@
+class  Squarepattern
+{
+    public static void main(String args[])
+    {
+        int i,j;
+        int n=5;
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n;j++)
+            {
+                if(i==1||i==n||i<=n||j==1||j==n||j<=n)
+                {
+                    System.out.print("*");
+                }
+                else{
+                    System.out.println(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
